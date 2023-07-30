aca5b7d802692d9e96edda1e1b9f047a6e501308
Michael Sebero <106393611+MichaelSebero@users.noreply.github.com>
2023-07-30 15:35:05 -0400

    Updated

--- files/coolrune-update/coolrune-update.sh
+++ files/coolrune-update/coolrune-update.sh
@@ -5,7 +5,8 @@
 read -rep $'Make sure to backup your passwords and bookmarks before updating CoolRune, press [ENTER] to continue. '
 
 su -c '
-root_files="curl -LO https://github.com/MichaelSebero/CoolRune/raw/main/files/coolrune-packages/coolrune-root.7z" 
-dotfiles="curl -LO https://github.com/MichaelSebero/CoolRune/raw/main/files/coolrune-packages/coolrune-dotfiles.7z"
+root_files="wget https://github.com/MichaelSebero/CoolRune/raw/main/files/coolrune-packages/coolrune-root.7z" 
+dotfiles="wget https://github.com/MichaelSebero/CoolRune/raw/main/files/coolrune-packages/coolrune-dotfiles.7z"
+coolrune-manual="wget https://raw.githubusercontent.com/MichaelSebero/CoolRune/main/files/coolrune-manual/CoolRune-Manual"
 
-pacman -Syyu --noconfirm --needed && mkdir /home/coolrune-files && cd /home/coolrune-files && eval $root_files && eval $dotfiles && chattr -i /etc/hosts && chattr -i /etc/resolv.conf && 7z x coolrune-root.7z -o/ -y && 7z x coolrune-dotfiles.7z -o/home/$USER -y && chattr +i /etc/hosts && chmod 777 /home/$USER/.var/io.github.celluloid_player.Celluloid -R && rm -rf /home/coolrune-files && rm -rf /home/$USER/coolrune-update.sh && chmod 777 /home/$USER/.librewolf -R && chmod 755 /home/$USER/.config -R && chmod 777 /home/$USER/.var/app -R && chattr +i /etc/resolv.conf && chmod 777 /home/$USER/.local/share/applications -R && grub-install && update-grub && reboot '
+pacman -Syyu --noconfirm --needed && mkdir /home/coolrune-files && cd /home/$USER/Desktop && eval $coolrune-manual && cd /home/coolrune-files && eval $root_files && eval $dotfiles && chattr -i /etc/hosts && chattr -i /etc/resolv.conf && 7z x coolrune-root.7z -o/ -y && 7z x coolrune-dotfiles.7z -o/home/$USER -y && chattr +i /etc/hosts && chmod 777 /home/$USER/.var/io.github.celluloid_player.Celluloid -R && rm -rf /home/coolrune-files && rm -rf /home/$USER/coolrune-update.sh && chmod 777 /home/$USER/.librewolf -R && chmod 755 /home/$USER/.config -R && chmod 777 /home/$USER/.var/app -R && chattr +i /etc/resolv.conf && chmod 777 /home/$USER/.local/share/applications -R && grub-install && update-grub && reboot '
